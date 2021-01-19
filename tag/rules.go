deccd4fd25378fd532140399354aef566a858a29
soranoba <soranoba@gmail.com>
2021-01-19 19:07:34 +0900

    validate tag initial implementation

--- tag/rules.go
+++ tag/rules.go
@@ -4,10 +4,14 @@ import (
 	"github.com/soranoba/valis"
 	"github.com/soranoba/valis/is"
 	"strconv"
+	"strings"
 )
 
 var (
+	// Required is the `required` tag rule.
+	//   - `required:"${flag}"`: When flag is true, the value must be not empty.
 	Required = valis.NewTagRule("required", requiredRules)
+	Validate = valis.NewTagRule("validate", validateRules)
 )
 
 func requiredRules(tagValue string) []valis.Rule {
@@ -16,4 +20,18 @@ func requiredRules(tagValue string) []valis.Rule {
 		return []valis.Rule{is.Required}
 	}
 	return []valis.Rule{}
+}
+
+func validateRules(tagValue string) []valis.Rule {
+	elems := strings.Split(tagValue, ",")
+	rules := make([]valis.Rule, 0)
+
+	for _, elem := range elems {
+		if elem == "-" {
+			return []valis.Rule{}
+		} else if elem == "required" {
+			rules = append(rules, is.Required)
+		}
+	}
+	return rules
 }
--- tests/tag/rules_test.go
+++ tests/tag/rules_test.go
@@ -1,7 +1,50 @@
 package tag_test
 
-import "testing"
+import (
+	"github.com/soranoba/valis"
+	valistag "github.com/soranoba/valis/tag"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var (
+	v *valis.Validator
+)
+
+func init() {
+	v = valis.NewValidator()
+	v.SetCommonRules()
+}
 
 func TestRequired(t *testing.T) {
+	assert := assert.New(t)
 
+	type User struct {
+		FirstName string `required:"true"`
+		LastName string `required:"false"`
+		Age int `required:"True"`
+	}
+
+	assert.EqualError(
+		v.Validate(User{}, valistag.Required),
+		"(required) .FirstName cannot be blank, but got \"\". (required) .Age cannot be blank, but got 0",
+	)
+	assert.NoError(
+		v.Validate(&User{FirstName: "Taro", LastName: "Soto", Age: 20}, valistag.Required),
+	)
+}
+
+func TestValidate_required(t *testing.T) {
+	assert := assert.New(t)
+
+	type User struct {
+		Name string `validate:"required"`
+	}
+	assert.EqualError(
+		v.Validate(User{}, valistag.Validate),
+		"(required) .Name cannot be blank, but got \"\"",
+	)
+	assert.NoError(
+		v.Validate(&User{Name: "Alice"}, valistag.Validate),
+	)
 }
